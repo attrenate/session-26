298607b237656cdb02770ca224f7fb0b6ff23f3a
attrenate <nikolozghomidzevol100@gmail.com>
2025-04-20 22:44:13 +0400

    added exercises

--- session26.js
+++ session26.js
@@ -12,4 +12,7 @@ const pokemons = [{name: 'Pikachu', CP: 320}, {name: 'Charizard', CP: 900}];
 const highest = pokemons.filter(pokemon => pokemon.CP > 500);
 console.log(highest);
 
-// 
+// 3. Avengers Assemble (Reduce)
+const Avengers = [{hero: 'Iron Man', budget: 5000}, {hero: 'Hulk', budget: 2000} ];
+const totalPower = Avengers.reduce((acc, curr) => acc + curr.budget, 0);
+console.log(totalPower); // 7000
